1702a4ddac8a1d9da7820c916093cea1d4b88792
Davi-Sobral <davissobral300909@gmail.com>
2026-05-10 22:43:06 -0300

    Versão 0.2 animação de entrada

---
+++ inicio.js
@@ -0,0 +1,6 @@
+const corpo = document.querySelector("main")
+const cabecario = document.querySelector("header")
+window.onload = function () {
+    corpo.classList.add("cabecario")
+    cabecario.classList.add("cabecario")
+}
